1cad10f629e5d0abd7f35dbf2671769157c5cb33
Devanshu Mahapatra <93381397+Devanshu-17@users.noreply.github.com>
2023-04-16 11:45:43 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", (e) => {
+  document.documentElement.style.setProperty("--scrollTop", `${this.scrollY}px`)
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+ScrollSmoother.create({
+  wrapper: ".wrapper",
+  content: ".content",
+})
